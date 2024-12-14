65ea4e24c38b312101c68ff1d2572f370fbae1e9
Steven Brown <steven.brown@bliztek.com>
2024-12-14 09:48:04 -0500

    Update readme with badges. Add CI workflow.

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/tests/**/*.test.ts"], // Matches your test files
-  moduleFileExtensions: ["ts", "js"],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // Optional, for global setups
-};
